392bfc56eb436c74260e91347849cbbf827935fa
jaffarabbas <gamaportal8@gmail.com>
2020-09-28 18:38:53 +0500

    28-92-2020  message dilog added

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,17 @@
 function StartChat(id){
     document.getElementById('ChatPanel').removeAttribute('style');
     document.getElementById('divStart').setAttribute('style','display:none');
+    hideChatList();
+}
+
+///////////////////////////////
+
+function showChatList(){
+    document.getElementById('side-1').classList.remove('d-none','d-md-block');
+    document.getElementById('side-2').classList.add('d-none');
+}
+
+function hideChatList(){
+    document.getElementById('side-1').classList.add('d-none','d-md-block');
+    document.getElementById('side-2').classList.remove('d-none');
 }
