a19d4219356150febe61099d401b57c813158926
jaffarabbas <gamaportal8@gmail.com>
2021-07-26 17:11:43 +0500

    7-26-2021     update

---
+++ js/firsebase.init.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js"></script>
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAcB9bKNGtP35oaqahlO6QURecXe4Bwc_8",
+  authDomain: "jzchattapp.firebaseapp.com",
+  projectId: "jzchattapp",
+  storageBucket: "jzchattapp.appspot.com",
+  messagingSenderId: "96949369157",
+  appId: "1:96949369157:web:b273c83068307c10a0a62c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
